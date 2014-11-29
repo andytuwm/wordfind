552f39312f0c018c80b9da74d82ca56a6b19449a
Andy Tu <andytu@alumni.ubc.ca>
2014-11-29 12:50:02 -0800

    Change Entry to use the separate Coordinates class instead now

--- src/wordfind/Entry.java
+++ src/wordfind/Entry.java
@@ -2,34 +2,13 @@ package wordfind;
 
 import java.util.List;
 
-public class Entry<A, B> {
-
-	/**
-	 * Quick and dirty coordinates class.
-	 * 
-	 * @author Andy
-	 *
-	 */
-	private class coordinates {
-		int x;
-		int y;
-		protected coordinates(int row, int column) {
-			x = row;
-			y = column;
-		}
-		protected int getRow() {
-			return x;
-		}
-		protected int getColumn() {
-			return y;
-		}
-	}
+public class Entry {
 
 	private String word;
-	private List<coordinates> path;
+	private List<Coordinates> path;
 	private int maxVert, minVert;
 	
-	public Entry ( String word, List <coordinates> passedPath){
+	public Entry ( String word, List <Coordinates> passedPath){
 		this.word = word;
 		path.addAll(passedPath);
 		
