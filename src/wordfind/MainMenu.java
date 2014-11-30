718f1a1c08aadb4568dafd2d1b1859dc9a599917
Andy Tu <andytu@alumni.ubc.ca>
2014-11-30 13:18:39 -0800

    remove solvefrom and change help description

--- src/wordfind/MainMenu.java
+++ src/wordfind/MainMenu.java
@@ -50,27 +50,9 @@ public class MainMenu {
 				System.out.println("Showing " + bottomLimit + " to "
 						+ showLimit + " of " + entries.size() + " words.");
 				for (int i = bottomLimit; i < showLimit; i++) {
-					System.out.println(entries.get(i).getWord());
+					System.out.println(entries.get(i).getWord() + "\tIncrease: " + entries.get(i).getMaxIncrease());
 				}
 				break;
-
-			/*
-			 * case "solvefrom":
-			 * System.out.println("Enter x coordinate (from 0 to " +
-			 * (brd.getColumns() - 1) + "):"); int x = reader.nextInt();
-			 * System.out.println("Enter y coordinate(from 0 to " +
-			 * (brd.getRows() - 1) + "):"); int y = reader.nextInt();
-			 * Coordinates c = new Coordinates(brd.getRows() - y, x); if (x < 0
-			 * || x > brd.getColumns() - 1 || y < 0 || y > brd.getRows() - 1) {
-			 * System.out.println("Invalid Coordinates."); break; } for
-			 * (Coordinates coord : brd.getStarts()) { boolean check = false; if
-			 * (!coord.equals(c)) { check = true; } if (check) {
-			 * System.out.println("Invalid Coordinates."); break; } } int
-			 * occurences = 0; for (int count = 0; occurences < 20; count++) {
-			 * if (entries.get(count).getPath().get(0).equals(c)) {
-			 * System.out.println(entries.get(count).getWord()); occurences++; }
-			 * } break;
-			 */
 
 			case "next":
 				bottomLimit += 20;
@@ -198,14 +180,13 @@ public class MainMenu {
 		System.out.println("\nCommands Available:");
 		System.out
 				.println("help - displays this help menu.\n"
-						+ "solve - rebuild board from file and solve again.\n"
-						+ "solveFrom - show solutions from the given coordinate. Shows only 20 entries.\n"
+						+ "solve - Show longest words you can play. Relative increase towards opponent\n\tbase is shown.\n"
 						+ "next - display next 20 solutions.\n"
 						+ "back - display last 20 solutions.\n"
 						+ "analyzeBestReach - show best words that allow for maximum distance gain towards \n\t\t   opponent's base.\n"
 						+ "analyzePosition - show words closest to opponent's base.\n"
 						+ "analyzeWin - show words that allow  you to win - processing may take time.\n"
-						+ "analyzeOpponent - show the most dangerous tiles in opponent's possession with \n\t\t  their longest words.\n"
+						+ "analyzeOpponent - show words the opponent can potentially win with and their\n\t\t  respective rows.\n"
 						+ "setBoard - set board file to build from; txt file must be in the folder files.\n"
 						+ "quit - end program.");
 	}
