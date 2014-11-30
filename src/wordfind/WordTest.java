a13024552f60aca7a67d4e5e7af06054bcfc53d9
Andy Tu <andytu@alumni.ubc.ca>
2014-11-29 17:33:39 -0800

    Basic JUnit test file

--- src/wordfind/WordTest.java
+++ src/wordfind/WordTest.java
@@ -16,9 +16,20 @@ public class WordTest {
 		dictionary = new Dictionary();
 	}
 	
-	@Test
+	//@Test
 	public void testDictionary() throws IOException {
 		dictionary.buildDictionary("files/dict.txt");
+	}
+	
+	@Test
+	public void testSolve() throws IOException {
+		dictionary.buildDictionary("files/dict.txt");
+		List<Entry> entries = new ArrayList<Entry>();
+		Board brd = new Board();
+		
+		entries = brd.solveBoard(dictionary, "files/board.txt");
+		
+		System.out.println(entries);
 	}
 	
 }
