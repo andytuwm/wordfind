3f9b1e67c7e5fee4441edf263d394ec07a0eb9b7
Andy Tu <andytu@alumni.ubc.ca>
2014-11-30 11:13:13 -0800

    reverse offsetcomparator order

--- src/comparators/OffsetComparator.java
+++ src/comparators/OffsetComparator.java
@@ -7,10 +7,10 @@ public class OffsetComparator implements java.util.Comparator<Entry> {
 	@Override
 	public int compare(Entry e1, Entry e2) {
 		if (e1.getOffset() > e2.getOffset())
-			return -1;
+			return 1;
 
 		if (e2.getOffset() > e1.getOffset())
-			return 1;
+			return -1;
 
 		return 0;
 	}
